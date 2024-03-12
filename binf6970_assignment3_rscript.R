835abcb080b4a4776ae981b4c8c96338ad174e6e
HamSlice7 <jhambly@uoguelph.ca>
2024-03-12 18:02:06 -0400

    Looking at the summary statistics

--- binf6970_assignment3_rscript.R
+++ binf6970_assignment3_rscript.R
@@ -2,3 +2,10 @@
 library(readxl)
 covid_data <- read_excel("Immunologic profiles of patients with COVID-19.xlsx")
 View(covid_data)
+
+#changing covid_data to data frame class
+covid_data <- as.data.frame(covid_data)
+
+#Looking at some summary statistics
+summary(covid_data)
+
