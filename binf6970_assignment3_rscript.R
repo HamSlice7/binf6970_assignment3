4a8d1b77ca6f3c0bd35c7a28e0e3e788f87531b3
HamSlice7 <jhambly@uoguelph.ca>
2024-03-13 18:11:53 -0400

    working up to model building

--- binf6970_assignment3_rscript.R
+++ binf6970_assignment3_rscript.R
@@ -1,16 +1,25 @@
-###Data Exploration 
 library(readxl)
+library(ggplot2)
+library(GGally)
+library(glmnet)
+
+#loading in the data
 covid_data <- read_excel("Immunologic profiles of patients with COVID-19.xlsx")
 
+###Data Exploration 
 #looking at the structure of covid_data
 str(covid_data)
 
+#looking at the correlation matrix between the numeric features
+correlation <- cor(covid_data[,c(-1,-3,-4)]) 
 
 #checking the class of each feature
 sapply(covid_data, class)
 
 #Looking at some summary statistics
 summary(covid_data)
+
+
 
 #viewing the dimensions of the data set
 dim(covid_data)
@@ -52,7 +61,34 @@ mean_severe <- as.data.frame(sapply(covid_data_severe, mean))
 abs(mean_mild - mean_severe)
 
 #z-score normalization
-covid_data_scale <- apply(covid_data_2, 2, scale)
+covid_data_standardized <- cbind(covid_data[,c(1,3,4)], (sapply(covid_data[,c(-1,-3,-4)], scale)))
 
-apply(covid_data_scale, 2, hist)
+#mean of covid_data_standardized
+sapply(covid_data_standardized, mean)
+
+#standard deviation of covid_data_standardized
+sapply(covid_data_standardized, sd)
+
+
+##formatting the predictors and response
+
+x <- covid_data_standardized[,c(-1,-3)]
+x$SEX <- ifelse(x$SEX == 'M', 1, 0)
+x$SEX <- as.factor(x$SEX)
+
+y <- ifelse(covid_data_standardized$Severirty == "Mild", 0, 1)
+table(y)
+
+###Train-test
+set.seed(1717)
+
+#generating indexes for the test set
+test_index <- sample(dim(x)[1],round(dim(x)[1] *.25), replace = FALSE )
+
+#train model - 10 folds
+alphas <- seq(0,1,0.01)
+
+for (i in alphas) {
+  cvx <- cv.glmnet(x[-test_index,], y[-test_index,], nfolds = 10, family = "binomial", alpha = i, type.measure = "auc" )
+}
 
